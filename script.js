135a71ccebdba4b93fc7dbffa551194b801fe6fa
srinivasa-rs <69103790+srinivasa-rs@users.noreply.github.com>
2026-02-01 21:54:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Portfolio loaded successfully");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 400;
+    const height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
+    }
+  });
+});
+
+sections.forEach(sec => {
+  sec.style.opacity = 0;
+  sec.style.transform = "translateY(50px)";
+  sec.style.transition = "0.8s ease";
+});
